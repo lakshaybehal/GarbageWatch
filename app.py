b84b0acf9c581cb87281d9e73894d77db69b30a7
unknown <vabeksroya9@gmail.com>
2026-05-20 08:32:50 +0530

    updated project

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'projectgarbagewatch@gmail.com'
-app.config['MAIL_PASSWORD'] = 'jqwr hnzc pprq qmix'  # no spaces
+app.config['MAIL_PASSWORD'] = 'iotm pihy ddwd ehrz'  # no spaces
 
 mail = Mail(app)
 
